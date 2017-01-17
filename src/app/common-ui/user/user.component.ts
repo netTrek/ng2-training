ab2623998adc975c958a26c26e56aa3125097a27
Saban Uenlue - netTrek <us@netTrek.de>
2017-01-17 23:21:09 +0100

    vorlagen

--- src/app/common-ui/user/user.component.ts
+++ src/app/common-ui/user/user.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'baywa-user',
-  templateUrl: './user.component.html',
+  template: `
+  <h3>User</h3>
+`,
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
