56563183428e9933108b1561f56c5628e6bd3ed3
Saban Uenlue - netTrek <us@netTrek.de>
2017-01-18 22:33:50 +0100

    from event End

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,24 +15,12 @@ export class AppComponent {
 
   constructor (  ) {
 
-    const observer : Subject<number> = new Subject<number>();
-    const observable : Observable<number> = observer.asObservable();
-
-
-    const subscription : Subscription = observable
-      .filter ( val => { return val % 2 === 0; } ) //nur gerade
-      .map ( item => item += 1 )                   //ungerade machen
+    const moveObservable : Observable<MouseEvent> = Observable.fromEvent<MouseEvent>(document, 'mousemove');
+    const subscription : Subscription = moveObservable
+      .filter ( evt => { return evt.clientX < 100; } )
       .subscribe(
-        next => console.info ( 'next %s', next ),
-        error => console.info ( 'error %s', error ),
-        () => console.info ( 'complete' ),
+        evt => console.info ( evt )
       );
-
-    observer.next( 1 );
-    observer.next( 2 );
-    observer.next( 3 );
-    observer.complete();
-    subscription.unsubscribe();
   }
 
 
