8b5ba0f11ff97db56232bb7963aa1d61d85b696c
Saban Uenlue - netTrek <us@netTrek.de>
2017-01-18 03:30:55 +0100

    end with binds

--- src/app/common-ui/italic.directive.ts
+++ src/app/common-ui/italic.directive.ts
@@ -1,12 +1,22 @@
-import {Directive, HostBinding} from '@angular/core';
+import {Directive, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[baywaItalic]'
 })
 export class ItalicDirective {
 
-  @HostBinding('class.italic') isItalic: boolean = true;
+  @HostBinding('class.italic') isItalic: boolean = false;
 
   constructor() { }
+
+  @HostListener('mouseenter')
+  onMouseEnter () {
+    this.isItalic = true;
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.isItalic = false;
+  }
 
 }
