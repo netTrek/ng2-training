5b134770d7b447ce22d6c06b55f55e9bf73017c2
Saban Uenlue - netTrek <us@netTrek.de>
2017-02-06 22:56:57 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'baywa-root',
+  selector: 'gfk-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
