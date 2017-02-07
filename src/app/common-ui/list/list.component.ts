3eba369bbe3dff8354d1e0997e9a36af75bf7488
Saban Uenlue - netTrek <us@netTrek.de>
2017-02-07 21:54:33 +0100

    end

--- src/app/common-ui/list/list.component.ts
+++ src/app/common-ui/list/list.component.ts
@@ -1,6 +1,5 @@
 import {
-  Component, OnInit, ViewEncapsulation, ViewChild, AfterViewInit, ContentChild,
-  AfterContentInit, ContentChildren, QueryList
+  Component, OnInit, ViewChild, ViewChildren, QueryList, OnChanges, AfterContentInit, AfterViewInit, ElementRef
 } from '@angular/core';
 import { ListTitleComponent } from './list-title/list-title.component';
 import { ListItemComponent } from './list-item/list-item.component';
@@ -10,22 +9,45 @@ import { ListItemComponent } from './list-item/list-item.component';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit, AfterContentInit {
+export class ListComponent implements OnInit, AfterViewInit {
 
-  @ContentChild (ListTitleComponent)
+  private _listItems: QueryList<ListItemComponent>;
+  
+  @ViewChild( ListTitleComponent )
   listTitle: ListTitleComponent;
 
-  @ContentChildren (ListItemComponent)
-  listItems: QueryList<ListItemComponent>;
+  @ViewChildren( ListItemComponent )
+  set listItems ( value : QueryList<ListItemComponent> ) {
+    this._listItems = value;
+    console.log ( 'set listItems', value );
+  }
 
+  @ViewChild ('testwert')
+  testKnoten: ElementRef;
+
+
+  val1: string = 'Hello world!';
+  num1: number= 111;
+
+  imgAlt: string = 'cats';
+  imgUrl: string = 'https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/30423_pets-products_january-site-flip_3-cathealth_short-tile_592x304._CB286975940_.jpg';
 
   constructor() { }
+
+  sayHello (): string {
+    return '<strong>say</strong> Hello <script>alert("hello");</script>';
+  }
+
+
 
   ngOnInit() {
   }
 
-  ngAfterContentInit () : void {
-    console.log ( this.listTitle, this.listItems );
+
+
+  ngAfterViewInit () : void {
+    console.log ( this.listTitle, this._listItems );
+    console.log ( 'testKnoten', this.testKnoten, this.testKnoten.nativeElement );
   }
 
 }
