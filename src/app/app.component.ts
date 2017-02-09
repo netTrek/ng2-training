fbc0ec78b818e876e6d28357233f74861219db33
Saban Uenlue - netTrek <us@netTrek.de>
2017-02-09 10:59:07 +0100

    stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { ReversePipe } from './common-ui/reverse.pipe';
-import { Observable, Subscription } from 'rxjs/RX';
+import { Observable, Subscription, Subject, BehaviorSubject } from 'rxjs/RX';
 
 export interface IUser {
     firstname : string;
@@ -21,6 +21,9 @@ export class AppComponent {
     selectedInd : number = 3;
 
     columns : string [] = [ 'age', 'firstname' ];
+
+    private usersObserver : BehaviorSubject<any> = new BehaviorSubject<any> ( null );
+            users$ : Observable<any>             = this.usersObserver.asObservable ();
 
     dataprovider : IUser [] = [
         {
@@ -48,47 +51,55 @@ export class AppComponent {
     listTitle : string = 'list title';
 
     constructor ( $http : Http ) {
+        /*
+        const observable : Observable<MouseEvent> = Observable.fromEvent ( document, 'mousemove' );
+        const subscription : Subscription         = observable
+            .filter ( evt => evt.clientX < 100 )
+            .subscribe (
+                next => console.log( 'next mouse event', next )
+            );
+        */
 
         //const observable : Observable<number> = Observable.range ( 1, 5 );
         //const observable : Observable<number> = Observable.of ( 1, 2, 3, 4, 4,  5 );
+        /*
+         const observable : Observable<number> = Observable.create (
+         observer => {
+         observer.next ( 1 );
+         observer.next ( 2 );
+         observer.error ( 'so nicht!' );
+         observer.next ( 3 );
+         observer.complete();
+         }
+         );
+         */
+        /*
+         const observer : Subject<number> = new Subject<number> ();
+         const observable : Observable<number> = observer.asObservable();
 
-        const observable : Observable<number> = Observable.create (
-            observer => {
-                observer.next ( 1 );
-                observer.next ( 2 );
-                observer.error ( 'so nicht!' );
-                observer.next ( 3 );
-                observer.complete();
-            }
-        );
-        const subscription : Subscription     = observable
-            //.filter ( val => val % 2 === 0 )
-            /*
-             .filter ( (val) => {
-             return val % 2 === 0;
-             })
-             */
-            //.map ( val => val += 1 )
-            /*
-             .map ( (val) => {
-             return val + 1;
-             } )
-             */
-            .subscribe (
-                next => console.log ( 'next', next ),  //next
-                error => console.log ( 'error', error ),  //error
-                /*
-                 ( next ) => {
-                 console.log ( next );
-                 },  //next
-                 ( error ) => {
-                 console.log ( error );
-                 },  //error
-                 */
-                () => {
-                    console.log ( 'complete' );
-                }  //complete
-            );
+         const subscription : Subscription     = observable
+         .subscribe (
+         next => console.log ( 'next', next ),  //next
+         error => console.log ( 'error', error ),  //error
+         () => {
+         console.log ( 'complete' );
+         }  //complete
+         );
+
+         observer.next( 1 );
+         observer.next( 2 );
+         console.log ( 'sub', subscription );
+         observer.next( 3 );
+
+         if ( ! subscription.closed ) {
+         subscription.unsubscribe();
+         }
+
+         observer.next( 4 );
+         observer.complete();
+         console.log ( 'sub', subscription );
+
+         */
 
         // subscription.unsubscribe();
 
@@ -99,11 +110,17 @@ export class AppComponent {
         // console.log ( myReversePipe.transform( '123' ) );
         // console.log ( myReversePipe.transform( [1,2,3] ) );
 
+        this.usersObserver.next ( { username: 'netTrek', age: 12 } );
+
     }
 
     chgInd ( newInd : number ) {
         // console.log ( 'index changed', newInd );
         this.selectedInd = newInd;
+    }
+
+    updateStream () {
+        this.usersObserver.next ( { username: 'saban', age: 43 } );
     }
 
     outputGen ( val : IUser ) : string {
