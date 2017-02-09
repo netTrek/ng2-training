3b3b52df3add1503f8a0d5138b353ed1cafcf54c
Saban Uenlue - netTrek <us@netTrek.de>
2017-02-09 13:27:26 +0100

    aufgeräumt

--- src/app/common-ui/list/list.component.ts
+++ src/app/common-ui/list/list.component.ts
@@ -1,27 +1,13 @@
 import {
-    Component, OnInit, ViewChild, ViewChildren, QueryList, OnChanges, AfterContentInit, AfterViewInit, ElementRef,
-    Input, Output, EventEmitter
+    Component, OnInit, AfterViewInit, Input, Output, EventEmitter
 } from '@angular/core';
-import { ListTitleComponent } from './list-title/list-title.component';
-import { ListItemComponent } from './list-item/list-item.component';
 
 @Component ( {
     selector   : 'gfk-list',
     templateUrl: './list.component.html',
     styleUrls  : [ './list.component.scss' ]
 } )
-export class ListComponent implements OnInit, AfterViewInit {
-
-    private _listItems : QueryList<ListItemComponent>;
-
-    @ViewChild ( ListTitleComponent )
-    listTitle : ListTitleComponent;
-
-    @ViewChildren ( ListItemComponent )
-    set listItems ( value : QueryList<ListItemComponent> ) {
-        this._listItems = value;
-        // // // // // console.log ( 'set listItems', value );
-    }
+export class ListComponent implements OnInit {
 
     @Input ()
     valueList : any[];
@@ -31,21 +17,6 @@ export class ListComponent implements OnInit, AfterViewInit {
 
     @Input ()
     parse : Function;
-
-    /*
-     @ViewChild ( 'testwert' )
-     testKnoten : ElementRef;
-     */
-
-    val1 : string = 'Hello world!';
-    num1 : number = 111;
-
-    size : number = 30;
-
-    imgAlt : string = 'cats';
-    imgUrl : string = '../../../assets/images/cat.jpg';
-
-    selectedClass : string = 'selected';
 
     @Input ()
     selectedUsrInd : number = 1;
@@ -60,10 +31,6 @@ export class ListComponent implements OnInit, AfterViewInit {
     titel : string = 'My List';
 
     constructor () {
-    }
-
-    sayHello () : string {
-        return '<strong>say</strong> Hello <script>alert("hello");</script>';
     }
 
     selectUsr ( usrInd : number ) {
@@ -82,11 +49,6 @@ export class ListComponent implements OnInit, AfterViewInit {
     }
 
     ngOnInit () {
-    }
-
-    ngAfterViewInit () : void {
-        // // // // // console.log ( this.listTitle, this._listItems );
-        // // // // // // console.log ( 'testKnoten', this.testKnoten, this.testKnoten.nativeElement );
     }
 
 }
