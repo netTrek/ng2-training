746d080cc177662de30729c1c3bbe00e56195d7d
Saban Uenlue - netTrek <us@netTrek.de>
2017-05-29 17:26:37 +0200

    bis einschließlich seite 76

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,33 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+@Component ( {
+    selector   : 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls  : [ './app.component.scss' ]
+} )
 export class AppComponent {
-  title = 'saban works!';
-  age = 12;
+    title    = 'saban works!';
+    cssclass = 'saban';
+    label    = 'schönes bild';
+    age      = 12;
+    color    = 'blue';
+    showSaban= false;
 
-  constructor () {
-    console.log( `root comp with age:= ${this.age} `);
-    console.log( `root comp with age:= ${this.age} `);
-    console.log( `root comp with age:= ${this.age} `);
-  }
+    imageSource = '../assets/images/logo-gross.png';
+
+    constructor () {
+        console.log ( `root comp with age:= ${this.age} ` );
+        console.log ( `root comp with age:= ${this.age} ` );
+        console.log ( `root comp with age:= ${this.age} ` );
+    }
+
+    chgAge ( newAge: number ) {
+        this.age = newAge;
+    }
+
+    calcBirthYear (): number {
+        const today: Date  = new Date ();
+        const year: number = today.getFullYear ();
+        return year - this.age;
+    }
 }
