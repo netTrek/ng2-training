c6125ebfbe7bfc5c7dad8867820f4c503dcb6476
Saban Uenlue - netTrek <us@netTrek.de>
2017-07-05 14:56:40 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pta-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'SABAN works!';
 }
