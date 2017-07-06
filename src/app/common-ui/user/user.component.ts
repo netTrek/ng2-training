52ee9da1b55b38881a9dd2bc13ddaf4f2007b310
Saban Uenlue - netTrek <us@netTrek.de>
2017-07-06 11:34:41 +0200

    inline style und host

--- src/app/common-ui/user/user.component.ts
+++ src/app/common-ui/user/user.component.ts
@@ -3,7 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'pta-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styles: [`
+    :host {
+      display: block;
+      background-color: #ffe31b;
+      color: red;
+    }
+    .spielen {
+      color: black;
+    }
+  `]
 })
 export class UserComponent implements OnInit {
 
