3b82df13018ffe2889d9275a81b972fa11ed8bdb
Saban Uenlue - netTrek <us@netTrek.de>
2017-07-06 16:48:55 +0200

    inpout output

--- src/app/common-ui/user-list/user-list.component.ts
+++ src/app/common-ui/user-list/user-list.component.ts
@@ -10,57 +10,24 @@ import { UserListHeaderComponent } from '../user-list-header/user-list-header.co
   templateUrl: './user-list.component.html',
   styleUrls  : [ './user-list.component.css' ]
 } )
-export class UserListComponent implements OnInit, AfterContentInit, AfterViewInit {
+export class UserListComponent implements OnInit {
 
-  zumSpielen = '123 Test';
-  zumSpielenHTML = '<strong>123</strong> Test';
-
-  bildQuelle = './assets/images/logo-gross.png';
-  bildInfo = 'logo von netTrek';
-
-  breite = 300;
-
-  /*
-   @ContentChild ( UserComponent )
-   user: UserComponent | undefined;
-
-   @ContentChildren ( UserComponent )
-   users: QueryList<UserComponent>;
-   */
-
-  @ViewChild ( UserListHeaderComponent )
-  header: UserListHeaderComponent;
-
-  @ViewChild ('subheader')
-  subheader: ElementRef;
-
-  fontcolor = 'red';
+  crrUserName = 'Heike';
 
   constructor () {
   }
 
   ngOnInit () {
-    console.log ( 'user list init' );
   }
 
-  ngAfterViewInit (): void {
-    console.log ( 'user list view init' );
-    console.log ( this.header );
-    console.log ( this.subheader );
-    this.subheader.nativeElement.style.color = this.fontcolor;
+  userClick( mouseEvt: MouseEvent ) {
+    console.log ('user was clicked', mouseEvt);
+    this.crrUserName = 'event ' + mouseEvt.clientX;
   }
 
-  ngAfterContentInit (): void {
-     console.log ( 'user list content init' );
-    /*
-     if ( this.user === undefined ) {
-     throw new Error ('User required');
-     } else {
-     this.user.username = 'saban';
-     console.log ( this.user );
-     }
-     console.log ( this.users );
-     */
+  handelNameChg ( name: string ) {
+    console.log ( 'select the user with name := ' + name );
   }
+
 
 }
--- src/app/common-ui/user/user.component.ts
+++ src/app/common-ui/user/user.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'pta-user',
   templateUrl: './user.component.html',
   styles: [`
     :host {
-      display: block;
+      display: inline-block;
       background-color: #ffe31b;
       color: red;
     }
@@ -14,14 +14,27 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
     }
   `]
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit,
+OnChanges {
 
+  @Input()
   username = 'Peter Müller';
 
+  @Output()
+  nameChg: EventEmitter<string> = new EventEmitter();
+
   constructor() { }
+
+  chgUsername() {
+    this.username = 'test';
+    this.nameChg.emit( this.username );
+  }
 
   ngOnInit() {
     console.log ( '#### user comp init' );
   }
 
+  ngOnChanges ( changes: SimpleChanges ): void {
+     console.log ( 'chg', changes );
+  }
 }
