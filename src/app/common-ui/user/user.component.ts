42708beb16dcc16cf7ca7d66e73708870298a073
Saban Uenlue - netTrek <us@netTrek.de>
2017-07-17 16:09:46 +0200

    ContentChild

--- src/app/common-ui/user/user.component.ts
+++ src/app/common-ui/user/user.component.ts
@@ -1,24 +1,32 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, OnInit } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
 
-@Component({
-  selector: 'app-user',
-  template: `<strong>Hello User</strong>
-  <p>Lorem ipsum dolor sit amet, consectetur</p>`,
-  styles: [`
-    :host {
-      background-color: azure;
-      display: inline-block;
-    }
-    p {
-      color: chartreuse;
-    }
-  `]
-})
-export class UserComponent implements OnInit {
+@Component ( {
+  selector   : 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls  : [ './user.component.css' ]
+} )
+export class UserComponent implements OnInit, AfterContentInit {
 
-  constructor() { }
+  @ContentChild ( LoginComponent )
+  loginComp: LoginComponent | undefined;
 
-  ngOnInit() {
+  constructor () {
   }
 
+  ngOnInit () {
+  }
+
+  ngAfterContentInit (): void {
+    console.log ( 'loginComp', this.loginComp );
+
+    if ( ! this.loginComp ) {
+      throw new Error ( 'Login Comp required' );
+    }
+
+    const old            = this.loginComp.label;
+    this.loginComp.label = 'Rock\'n Roll';
+    console.log ( `old:= [${old}] new := [${this.loginComp.label}]` );
+
+  }
 }
