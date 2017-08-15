62d1fa4f941052368fbe097611513bc5e3e64c3c
Saban Uenlue - netTrek <us@netTrek.de>
2017-08-15 11:04:42 +0200

    @ContentChild und Children

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, ContentChildren, ElementRef, OnInit, QueryList } from '@angular/core';
+import { UserNameComponent } from './user-name/user-name.component';
 
 @Component({
   selector: 'hb-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, AfterContentInit {
 
-  constructor() { }
+  @ContentChild ( UserNameComponent )
+  userName: UserNameComponent;
+
+  @ContentChildren ( UserNameComponent )
+  userNames: QueryList<UserNameComponent>;
+/*
+  !!!!!! SO NIE !!!!!!
+  constructor( elem: ElementRef ) {
+    console.log ( elem );
+    elem.nativeElement.getElementsByTagName( 'hb-user-name' );
+  }
+*/
+
+  constructor () {}
 
   ngOnInit() {
   }
 
+  ngAfterContentInit (): void {
+    console.log( 'alle content inhalte init', this.userName, this.userNames );
+  }
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
+import { UserNameComponent } from './user-name/user-name.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [UserComponent],
-  exports: [UserComponent]
+  declarations: [UserComponent, UserNameComponent],
+  exports: [UserComponent, UserNameComponent]
 })
 export class UserModule { }
