60a7c931d9a65ee8930e7117a3c80e2efb00d4d6
Saban Uenlue - netTrek <us@netTrek.de>
2017-08-16 09:53:13 +0200

    HostBinding und HostListener

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import {
-  AfterViewInit, /*AfterContentInit,*/ Component, ElementRef, EventEmitter, Input, OnChanges, /*ContentChild, ContentChildren, ElementRef,*/
+  AfterViewInit, /*AfterContentInit,*/ Component, ElementRef, EventEmitter, HostBinding, HostListener, Input,
+  OnChanges, /*ContentChild, ContentChildren, ElementRef,*/
   OnInit,
   Output,
   QueryList, SimpleChanges, ViewChild
@@ -7,13 +8,12 @@ import {
 import { UserNameComponent } from './user-name/user-name.component';
 import { UserDto } from '../model/user-dto';
 
-@Component({
-  selector: 'hb-user',
+@Component ( {
+  selector   : 'hb-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
-})
-export class UserComponent implements OnInit/*, AfterContentInit*/ , AfterViewInit, OnChanges {
-
+  styleUrls  : [ './user.component.scss' ]
+} )
+export class UserComponent implements OnInit/*, AfterContentInit*/, AfterViewInit, OnChanges {
 
   /*
 
@@ -24,51 +24,78 @@ export class UserComponent implements OnInit/*, AfterContentInit*/ , AfterViewIn
     userNames: QueryList<UserNameComponent>;
   */
 
-/*
-  !!!!!! SO NIE !!!!!!
-  constructor( elem: ElementRef ) {
-    console.log ( elem );
-    elem.nativeElement.getElementsByTagName( 'hb-user-name' );
-  }
-*/
+  /*
+    !!!!!! SO NIE !!!!!!
+    constructor( elem: ElementRef ) {
+      console.log ( elem );
+      elem.nativeElement.getElementsByTagName( 'hb-user-name' );
+    }
+  */
 
-  @Input()
+  @Input ()
   user: UserDto;
 
-  @Output()
-  selectUsr: EventEmitter<UserDto> = new EventEmitter();
+  @Output ()
+  selectUsr: EventEmitter<UserDto> = new EventEmitter ();
 
   @ViewChild ( UserNameComponent )
   userName: UserNameComponent;
-/*
-  @ViewChild ( 'usrheader' )
-  userHeader: ElementRef;
-*/
 
-  constructor () {}
+  /*
+    @ViewChild ( 'usrheader' )
+    userHeader: ElementRef;
+  */
 
-  setAsSelected () {
+  constructor () {
+  }
+
+  // [style.color]="color"
+  @HostBinding ( 'style.color' )
+  color = undefined;
+
+  // [style.background-color]="color"
+  @HostBinding ( 'style.backgroundColor' )
+  bgcolor = undefined;
+
+  // (mouseenter)="enter()"
+  @HostListener ( 'mouseenter' )
+  enter ( evt?: Event ) {
+    this.color   = 'red';
+    this.bgcolor = 'yellow';
+  }
+
+  // (mouseleave)="leave()"
+  @HostListener ( 'mouseleave' )
+  leave ( evt?: Event ) {
+    this.color   = undefined;
+    this.bgcolor = undefined;
+  }
+
+  @HostListener ( 'click', [ '$event' ] ) // <hb-user (click)="methode($event)"
+  setAsSelected ( evt?: MouseEvent ) {
+    console.log ( evt );
     // console.log ( 'set this user', this.user, 'as selected' );
-    this.selectUsr.next( this.user );
+    this.selectUsr.next ( this.user );
   }
 
   ngOnChanges ( changes: SimpleChanges ): void {
     console.log ( changes );
   }
 
-  ngOnInit() {
+  ngOnInit () {
   }
-/*
-  ngAfterContentInit (): void {
-    console.log( 'alle content inhalte init', this.userName, this.userNames );
-  }
-  */
+
+  /*
+    ngAfterContentInit (): void {
+      console.log( 'alle content inhalte init', this.userName, this.userNames );
+    }
+    */
 
   ngAfterViewInit (): void {
-  /*
-    console.log( 'alle template-inhalte init', this.userName/!*, this.userNames*!/ );
-    console.log ( this.userHeader );
-    this.userHeader.nativeElement.style.color = 'red';
-  */
+    /*
+      console.log( 'alle template-inhalte init', this.userName/!*, this.userNames*!/ );
+      console.log ( this.userHeader );
+      this.userHeader.nativeElement.style.color = 'red';
+    */
   }
 }
