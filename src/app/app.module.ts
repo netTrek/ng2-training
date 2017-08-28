8e8e3ce11dfce9fe2779d029e9ac6509e021e0c2
Saban Uenlue - netTrek <us@netTrek.de>
2017-08-28 14:57:13 +0200

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 
-
-@NgModule({
+@NgModule ( {
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports     : [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  providers   : [],
+  bootstrap   : [ AppComponent ]
+} )
+export class AppModule {
+}
 
