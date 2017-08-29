24ac2c70926b63945198c1c55764f358ac9bd1fe
Saban Uenlue - netTrek <us@netTrek.de>
2017-08-29 10:07:35 +0200

    userList

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'msg-user',
   templateUrl: './user.component.html',
+  // styles: [`
+  //          h2 {
+  //            color: red;
+  //          }
+  //          `]
   styleUrls: ['./user.component.scss']
+  // encapsulation: ViewEncapsulation.None
 })
 export class UserComponent implements OnInit {
 
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserHeaderComponent } from './user-header/user-header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [UserComponent],
-  exports: [UserComponent]
+  declarations: [UserComponent, UserListComponent, UserHeaderComponent],
+  exports: [UserComponent, UserListComponent, UserHeaderComponent]
 })
 export class UserModule { }
