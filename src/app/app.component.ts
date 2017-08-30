7e00f9a73df09a2fc4a8967f3929b2618141afd7
Saban Uenlue - netTrek <us@netTrek.de>
2017-08-30 14:05:05 +0200

    rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component, HostListener } from '@angular/core';
 import { UserDto } from './user/user-dto';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/range';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { log } from 'util';
 
 @Component ( {
   selector   : 'msg-root',
@@ -10,7 +20,7 @@ export class AppComponent {
 
   users: UserDto [] = [];
 
-  date: number = Date.now();
+  date: number = Date.now ();
 
   className = 'supercool';
 
@@ -35,10 +45,66 @@ export class AppComponent {
   };
   disableList                         = false;
 
+  currNo: number;
+
   constructor () {
 
-    
+    // const observable: Observable<number> = Observable.of ( 1, 2, 3, 4, 5 );
+    // const observable: Observable<number> = Observable.range( 2, 22 );
+/*
 
+    const observable: Observable<number> = Observable.create ( observer => {
+      observer.next( 1 );
+      observer.next( 2 );
+      // make xhr - http
+      // observer.error ( 'ups' );
+      observer.next( 3 );
+      window.setTimeout( () => {
+        observer.next( 4 );
+        observer.complete();
+      }, 1000 );
+    });
+*/
+    // const observable: Subject<number> = new Subject();
+    const observable: BehaviorSubject<number> = new BehaviorSubject(0);
+
+    observable.next( 1 );
+    observable.next( 2 );
+
+    const sub: Subscription = observable
+      // .filter ( val => val % 2 === 0 )
+      // .map ( val => val * 100 )
+      .subscribe(
+        next => console.log ( next ),
+        error => console.log ( error ),
+        () => {
+          console.log ('ioch habe fertig');
+        }
+      );
+
+
+    observable.next( 3 );
+    observable.next( 4 );
+
+    console.log ( sub );
+    const eventObservable: Observable<MouseEvent> = Observable.fromEvent ( document, 'mousemove');
+
+    eventObservable
+      .filter ( evt => evt.clientX < 100 )
+      .subscribe( evt => console.log( evt ) );
+
+/*
+    observable.subscribe(
+      ( next ) => {
+        console.log ( next );
+      },
+      ( error ) => {
+        console.log ( error );
+      },
+      () => {
+        console.log ('ioch habe fertig');
+      }
+    );*/
 
     for ( let i = 0; i < 10; i ++ ) {
       this.users.push ( <UserDto> {
@@ -46,7 +112,7 @@ export class AppComponent {
         lastname : `uenlue ${i}`,
         age      : 22 + i,
         city     : `dorste ${i}`,
-        zip: `46282 - ${i}`
+        zip      : `46282 - ${i}`
       } );
     }
     console.log ( this.users );
